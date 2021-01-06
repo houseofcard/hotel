a02080ec71f0d8439893c1dcc73c3230e0be6978
houseofcard <53417201+houseofcard@users.noreply.github.com>
2021-01-07 10:29:45 +1300

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,189 @@
+<link rel="stylesheet" type="text/css" href="styles/aboutus.css">
+
+<?php
+// core configuration
+include_once 'config/core.php';
+
+// headings
+include_once 'layout/template_header.php';
+include_once 'layout/template_booking.php';
+?>
+
+<div class="body_content">
+	<div class="container">
+		<div class="row row-eq-height">
+
+			<!-- Intro Content -->
+			<div class="col-lg-6">
+				<div class="intro_content">
+					<div class="section_title">
+						<div>Hello</div>
+						<h1>Amazing Hotel in front of the Sea</h1>
+					</div>
+					<div class="intro_text">
+						<p>Maecenas sollicitudin tincidunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scele risque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperdiet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus.</p>
+					</div>
+				</div>
+			</div>
+
+			<!-- Intro Image -->
+			<div class="col-lg-6">
+				<div class="intro_image">
+					<div class="background_image" style="background-image:url(images/aboutus/intro.jpg)"></div>
+					<img src="images/aboutus/intro.jpg" alt="">
+				</div>
+			</div>
+		</div>
+    </div>
+
+	<!-- Offering -->
+	<div class="offering">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title text-center">
+						<div>Resort</div>
+						<h1>What we offer</h1>
+					</div>
+				</div>
+			</div>
+			<div class="row offering_row">
+        
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image">
+							<img src="images/aboutus/offer_1.jpg" alt="">
+						</div>
+						<div class="offer_content text-center">
+							<div class="offer_title">
+								<h3>Outdoor Pool</h3>
+							</div>
+							<div class="offer_text">
+								<p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image">
+							<img src="images/aboutus/offer_2.jpg" alt="">
+						</div>
+						<div class="offer_content text-center">
+							<div class="offer_title">
+								<h3>Indoor Pool</h3>
+							</div>
+							<div class="offer_text">
+								<p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image">
+							<img src="images/aboutus/offer_3.jpg" alt="">
+						</div>
+						<div class="offer_content text-center">
+							<div class="offer_title">
+								<h3>Spa Zone</h3>
+							</div>
+							<div class="offer_text">
+								<p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image">
+							<img src="images/aboutus/offer_4.jpg" alt="">
+						</div>
+						<div class="offer_content text-center">
+							<div class="offer_title">
+								<h3>Sports Area</h3>
+							</div>
+							<div class="offer_text">
+								<p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image">
+							<img src="images/aboutus/offer_5.jpg" alt="">
+						</div>
+						<div class="offer_content text-center">
+							<div class="offer_title">
+								<h3>Restaurant</h3>
+							</div>
+							<div class="offer_text">
+								<p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Offer Item -->
+				<div class="col-xl-4 col-md-6">
+					<div class="offer">
+						<div class="offer_image">
+							<img src="images/aboutus/offer_6.jpg" alt="">
+						</div>
+						<div class="offer_content text-center">
+							<div class="offer_title">
+								<h3>Skybar</h3>
+							</div>
+							<div class="offer_text">
+								<p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Discover -->
+	<div class="discover">
+
+		<!-- Discover Content -->
+		<div class="discover_content">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-5">
+						<div class="section_title">
+							Hotel
+							<h1>Discover Marimar Hotel</h1>
+						</div>
+					</div>
+					<div class="row discover_row">
+						<div class="col-lg-5">
+							<div class="discover_highlight">
+								<p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur.</p>
+							</div>
+						</div>
+						<div class="col-lg-7">
+							<div class="discover_text">
+								<p>Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis nulla, id laoreet neque scelerisque at. Quisque eget sem non ligula consectetur ultrices in quis augue. Donec imperd iet leo eget tortor dictum, eget varius eros sagittis. Curabitur tempor dignissim massa ut faucibus sollicitudin tinci dunt maximus. Morbi tempus malesuada erat sed pellentesque. Donec pharetra mattis.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+	include_once 'layout/template_footer.php';
+?>
